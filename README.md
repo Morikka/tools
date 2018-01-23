49d6e5c6023620add21b4aadeb2578dee449653e
mio <9just.k.on@gmail.com>
2018-01-23 19:46:33 +0800

    add bot.py
    增加bot小程序

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 ## merge
 合成pdf - 具体内容见文件夹中的readme
 
+## bot.py
+Telegram上的bot小程序，用来发猫猫图
+link: http://t.me/cat_needed_bot
+
 ## convert
 待完成。。。
 
---
+++ bot.py
@@ -0,0 +1,33 @@
+import telepot
+from telepot.loop import MessageLoop
+import random
+import os
+import time
+
+bot = telepot.Bot('')
+bot.setWebhook()
+
+def rand():
+    ans = random.randint(0,749)
+    f = 'cat/'+str(ans)+'.jpg'
+    jud = os.access(f,os.F_OK)
+    print str(ans)+' '+str(jud)
+    if jud:
+        return ans
+    return rand()
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    if content_type == 'text':
+        command = msg['text']
+        if command.startswith('/money'):
+            bot.sendPhoto(chat_id,open('wx.jpg','rb'))
+        elif command.startswith('/cat'):
+            getphoto = rand()
+            bot.sendPhoto(chat_id,open('cat/'+str(getphoto)+'.jpg','rb'))
+
+MessageLoop(bot, handle).run_as_thread()
+
+while 1:
+    time.sleep(10)
